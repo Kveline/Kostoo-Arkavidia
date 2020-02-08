9e9e281f77a1059b69c8e368149e51dfc612401c
alfanadhya <alfa150800@gmail.com>
2020-02-08 15:49:07 +0700

    push

--- core/user.js
+++ core/user.js
@@ -1,66 +1,59 @@
 const pool = require("./pool");
 const bcrypt = require("bcryptjs");
 
-function User(){};
+function User() {};
 
 User.prototype = {
-    find: function (email, type, callback) {
+    find: function(email, type, callback) {
         let sql = "";
-        if(type === "desa"){
+        if (type === "desa") {
             sql = "SELECT * FROM desa WHERE email = ?";
-        }
-        else{
+        } else {
             sql = "SELECT * FROM investor WHERE email = ?";
         }
         pool.query(sql, email, (err, result) => {
-            if(err) throw err;
-            if(result.length){
+            if (err) throw err;
+            if (result.length) {
                 callback(result[0]);
-            }
-            else{
+            } else {
                 callback(null);
             }
         });
     },
-    login: function (email, password, type, callback) {
+    login: function(email, password, type, callback) {
         this.find(email, type, (result) => {
-            if(result){
-                if(bcrypt.compareSync(password, result.password)){
+            if (result) {
+                if (bcrypt.compareSync(password, result.password)) {
                     callback(result);
-                }
-                else{
+                } else {
                     callback(null);
                 }
-            }
-            else{
+            } else {
                 callback(null);
             }
         })
     },
-    create: function(email, pass, type, callback) {
+    create: function(email, pass, nama, lokasi, foto, deskripsi, portofolio, bidang, type, callback) {
         this.find(email, type, (result) => {
-            if(result){
+            if (result) {
                 callback(null);
-            }
-            else{
+            } else {
                 let password = bcrypt.hashSync(pass, 10);
                 let bind = [];
                 bind.push(email);
                 bind.push(password);
                 let sql = "";
-                if(type === "desa"){
+                if (type === "desa") {
                     sql = "INSERT INTO desa(email, password) VALUES (?, ?)";
-                }
-                else{
+                } else {
                     sql = "INSERT INTO investor(email, password) VALUES (?, ?)";
                 }
 
                 pool.query(sql, bind, (err, result) => {
-                    if(err) throw err;
-                    if(result){
+                    if (err) throw err;
+                    if (result) {
                         callback(result);
-                    }
-                    else{
+                    } else {
                         callback(null);
                     }
                 })
